acf0d5223c2769c9e275bbe65fcbf853df397d0f
SIYUAN GUO <uvasiyuan@gmail.com>
2016-03-03 21:39:44 -0500

    Added files via upload

--- findFamilies.py
+++ findFamilies.py
@@ -3,9 +3,13 @@
 import xlrd
 import sys
 import pickle
+import numpy
+import xlwt
 
 workbook = xlrd.open_workbook('/Users/Uvaguy/Desktop/ml/titannic/titanic3.xls')
 data=workbook.sheet_by_name('titanic3')
+
+# print(data);
 
 sibsp=[]
 parch=[]
@@ -29,32 +33,49 @@ while rowNum < len(sibsp):
         fam.append(rowNum)
     rowNum=rowNum+1
 
-print fam
-print nonfam
+# print fam
+# print nonfam
 
 final_fam=[]
 final_nonfam=[]
 
-#removing families that have members of unknown age
-for i in range(len(fam)):
-    addFamily=True
-    if data.cell(fam[i],4).value == xlrd.empty_cell.value:
-        print "Reject indivual at row "+str(fam[i]+1)+' entry!\n'
-        addFamily=False
-    if addFamily:
-        final_fam.append(fam[i])
+#replacing missing value with mean families that have members of unknown age
+average = []
+col = data.col(4)
+for i in range(1 , len(col)):
+    if col[i].value == xlrd.empty_cell.value:
+        col[i].value = 0
+    average.append(col[i].value)
+ave = numpy.mean(average)
 
-for i in range(len(nonfam)):
-    addFamily=True
-    if data.cell(nonfam[i],4).value == xlrd.empty_cell.value:
-        print "Reject indivual at row "+str(nonfam[i]+1)+' entry!\n'
-        addFamily=False
-    if addFamily:
-        final_nonfam.append(nonfam[i])
+#print ave
+
+# print col
+for i in range(len(col)):
+    if col[i].value == xlrd.empty_cell.value:
+        data.col(4)[i].value = ave
 
 
-print final_fam
-print final_nonfam
+def writetocsv(self, filename):
+    self.data.to_csv(filename,sep=',')
+
+# from xlwt import Workbook
+# wb = Workbook()
+# Sheet2 = wb.add_sheet(data)
+# wb.save('ex.xls')
+
+
+# for i in range(len(nonfam)):
+#     addFamily=True
+#     if data.cell(nonfam[i],4).value == xlrd.empty_cell.value:
+#         # data.cell(nonfam[i],4),value = average
+#         addFamily=True
+#     if addFamily:
+#         final_nonfam.append(nonfam[i])
+
+
+# print final_fam
+# print final_nonfam
 
 pickle.dump(final_fam,open('fam.pickle','wt'))
 pickle.dump(final_nonfam,open('nonfam.pickle','wt'))
